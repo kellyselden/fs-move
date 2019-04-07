eb940efbc49f20dc233a8ca0546ccf6d6736472d
Kelly Selden <kellyselden@gmail.com>
2019-04-07 20:28:17 +0100

    support callbacks

--- src/index.js
+++ src/index.js
@@ -10,71 +10,88 @@ const lstat = denodeify(fs.lstat);
 const readdir = denodeify(fs.readdir);
 const rmdir = denodeify(fs.rmdir);
 
-const _move = co.wrap(function* move(src, dest, options = {}) {
-  let {
-    overwrite,
-    merge,
-    filter = () => true
-  } = options;
-
-  if (!(yield Promise.resolve(filter(src, dest)))) {
-    return;
+const _move = co.wrap(function* move(src, dest, options = {}, callback) {
+  if (typeof options === 'function') {
+    callback = options;
+    options = {};
   }
-
-  let destStats;
 
   try {
-    destStats = yield lstat(dest);
-  } catch (err) {
-    // do nothing
-  }
+    let {
+      overwrite,
+      merge,
+      filter = () => true
+    } = options;
 
-  if (destStats && !overwrite && !merge) {
-    throw new Error('Destination directory already exists');
-  }
+    if (!(yield Promise.resolve(filter(src, dest)))) {
+      return;
+    }
 
-  let srcStats = yield lstat(src);
+    let destStats;
 
-  let areBothDirs = destStats && srcStats.isDirectory() && destStats.isDirectory();
-
-  // pre
-
-  if (overwrite && (!areBothDirs || !merge)) {
-    yield rimraf(dest);
-
-    destStats = null;
-  }
-
-  // during
-
-  if (!destStats) {
     try {
-      yield denodeify(fs.rename)(src, dest);
+      destStats = yield lstat(dest);
     } catch (err) {
-      if (err.code === 'EXDEV') {
-        yield cpr(src, dest);
+      // do nothing
+    }
 
-        yield rimraf(src);
+    if (destStats && !overwrite && !merge) {
+      throw new Error('Destination directory already exists');
+    }
+
+    let srcStats = yield lstat(src);
+
+    let areBothDirs = destStats && srcStats.isDirectory() && destStats.isDirectory();
+
+    // pre
+
+    if (overwrite && (!areBothDirs || !merge)) {
+      yield rimraf(dest);
+
+      destStats = null;
+    }
+
+    // during
+
+    if (!destStats) {
+      try {
+        yield denodeify(fs.rename)(src, dest);
+      } catch (err) {
+        if (err.code === 'EXDEV') {
+          yield cpr(src, dest);
+
+          yield rimraf(src);
+        }
       }
     }
-  }
 
-  if (merge && areBothDirs) {
-    for (let file of yield readdir(src)) {
-      yield _move(
-        path.join(src, file),
-        path.join(dest, file),
-        options
-      );
+    if (merge && areBothDirs) {
+      for (let file of yield readdir(src)) {
+        yield _move(
+          path.join(src, file),
+          path.join(dest, file),
+          options
+        );
+      }
     }
-  }
 
-  // post
+    // post
 
-  try {
-    yield rmdir(src);
+    try {
+      yield rmdir(src);
+    } catch (err) {
+      // do nothing
+    }
+
+    if (callback) {
+      callback(null);
+    }
   } catch (err) {
-    // do nothing
+    if (!callback) {
+      throw err;
+    }
+
+    callback(err);
   }
 });
 
